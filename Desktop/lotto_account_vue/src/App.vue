f0ae3b1c82a7a60e95bc619dadd8bd19e50d558b
hyozzi92 <gytjdzja@gmail.com>
2022-12-31 18:15:22 +0900

    Nav done

---
+++ Desktop/lotto_account_vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Nav></Nav>
+  <Carousel />
+</template>
+
+<script>
+import Nav from "./components/Nav.vue";
+import Carousel from "./components/Carousel.vue";
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Carousel,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
